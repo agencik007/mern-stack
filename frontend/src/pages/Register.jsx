2593dbf2f002879e5179ac4788b33b403c4234d6
agencik007 <agencik007@gmail.com>
2022-02-27 11:03:45 +0100

    Register is now working - frontend and backend

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,10 @@
-import {useState, useEffect} from "react";
-import {FaUser} from 'react-icons/fa'
+import { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { FaUser } from 'react-icons/fa'
+import { register, reset } from '../features/auth/authSlice'
+import {Spinner} from '../components/Spinner'
 
 export function Register() {
   const [formData, setFormData] = useState({
@@ -9,34 +14,69 @@ export function Register() {
     password2: '',
   })
 
-  const {name, email, password, password2} = formData;
+  const { name, email, password, password2 } = formData
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  )
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+
+    if (isSuccess || user) {
+      navigate('/')
+    }
+
+    dispatch(reset())
+  }, [user, isError, isSuccess, message, navigate, dispatch])
 
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value,
     }))
-  };
+  }
 
   const onSubmit = (e) => {
-    e.preventDefault();
-  };
+    e.preventDefault()
+
+    if (password !== password2) {
+      toast.error('Passwords do not match')
+    } else {
+      const userData = {
+        name,
+        email,
+        password,
+      }
+
+      dispatch(register(userData))
+    }
+  }
+
+  if (isLoading) {
+    return <Spinner />
+  }
 
   return (
     <>
-      <section className="heading">
+      <section className='heading'>
         <h1>
           <FaUser /> Register
         </h1>
         <p>Please create an account</p>
       </section>
 
-      <section className="form">
+      <section className='form'>
         <form onSubmit={onSubmit}>
-          <div className="form-group">
+          <div className='form-group'>
             <input
-              type="text"
-              className="form-control"
+              type='text'
+              className='form-control'
               id='name'
               name='name'
               value={name}
@@ -44,10 +84,10 @@ export function Register() {
               onChange={onChange}
             />
           </div>
-          <div className="form-group">
+          <div className='form-group'>
             <input
-              type="email"
-              className="form-control"
+              type='email'
+              className='form-control'
               id='email'
               name='email'
               value={email}
@@ -55,21 +95,21 @@ export function Register() {
               onChange={onChange}
             />
           </div>
-          <div className="form-group">
+          <div className='form-group'>
             <input
-              type="password"
-              className="form-control"
+              type='password'
+              className='form-control'
               id='password'
               name='password'
               value={password}
-              placeholder='Enter your password'
+              placeholder='Enter password'
               onChange={onChange}
             />
           </div>
-          <div className="form-group">
+          <div className='form-group'>
             <input
-              type="password"
-              className="form-control"
+              type='password'
+              className='form-control'
               id='password2'
               name='password2'
               value={password2}
@@ -77,8 +117,10 @@ export function Register() {
               onChange={onChange}
             />
           </div>
-          <div className="form-group">
-            <button type="submit" class="btn btn-block">Submit</button>
+          <div className='form-group'>
+            <button type='submit' className='btn btn-block'>
+              Submit
+            </button>
           </div>
         </form>
       </section>
